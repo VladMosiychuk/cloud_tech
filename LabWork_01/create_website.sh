c45954a3d68a9f2a74179b39057bd2b7101e6642
Vlad Mosiychuk <mosyka007@gmail.com>
2020-11-07 21:22:35 +0200

    Auto destructor + script improvement

--- LabWork_01/create_website.sh
+++ LabWork_01/create_website.sh
@@ -1,11 +1,24 @@
+#!/bin/bash
+
+# Bucket name
+BUCKET_NAME="itsu-lab-01-bucket"
+
+# Region to use
+AWS_REGION="us-east-1" 
+
 # Create bucket command:
-aws s3 mb s3://itsu-lab-01-bucket --region us-east-1
+aws s3 mb s3://$BUCKET_NAME --region $AWS_REGION
 
 # Upload files
-aws s3 sync website/ s3://itsu-lab-01-bucket/
+aws s3 sync website/ s3://$BUCKET_NAME/
 
 # Enable website
-aws s3api put-bucket-website --bucket itsu-lab-01-bucket --website-configuration file://website.json
+aws s3api put-bucket-website --bucket $BUCKET_NAME --website-configuration file://website.json
 
 # Setup bucket policy
-aws s3api put-bucket-policy --bucket itsu-lab-01-bucket --policy file://policy.json
+aws s3api put-bucket-policy --bucket $BUCKET_NAME --policy file://policy.json
+
+# Crete destructor
+echo "#!/bin/bash
+aws s3 rb s3://$BUCKET_NAME --region $AWS_REGION --force
+" > "destructor.sh"
--- LabWork_01/destructor.sh
+++
@@ -1,1 +0,0 @@
-aws s3 rb s3://itsu-lab-01-bucket --region us-east-1 --force
